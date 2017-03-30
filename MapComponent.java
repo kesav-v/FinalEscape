9efc0cb0a85d7ab55607301f22aed2bc34518975
Ofek Gila <ofek@theofekfoundation.org>
2017-03-29 21:20:00 -0700

    Add solid and opaque

--- MapComponent.java
+++ MapComponent.java
@@ -2,12 +2,14 @@ public abstract class MapComponent {
 
 	private int x, y;
 	private Map map;
+	private boolean solid, opaque;
 
 	public MapComponent(Map map, int x, int y) {
 		this.map = map;
 		this.x = x;
 		this.y = y;
 		map.addComponent(this);
+		solid = opaque = false;
 	}
 
 	public final void moveTo(int x, int y) {
@@ -19,4 +21,10 @@ public abstract class MapComponent {
 	public int getX() { return x; }
 	public int getY() { return y; }
 	public Map getMap() { return map; }
+
+	public boolean isSolid() { return solid; }
+	public boolean isOpaque() { return opaque; }
+
+	public void setSolid(boolean solid) { this.solid = solid; }
+	public void setOpaque(boolean opaque) { this.opaque = opaque; }
 }
