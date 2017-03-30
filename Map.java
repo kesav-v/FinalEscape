2371c2a50914fa879d0f8b36b1ca04cd8c398e6b
chess123 <kesav@viswanadha.net>
2017-03-29 19:26:01 -0700

    Maze class

--- Map.java
+++ Map.java
@@ -1,5 +1,29 @@
 import java.util.ArrayList;
 
 public class Map {
-	private MapComponent[][] occupantArray;
+	//private MapComponent[][] occupantArray;
+	private boolean[][] maze;
+
+	public Map(int s) {
+		int size = 4 * s + 1;
+		int randX = 2 * (int)(Math.random() * (size / 2 - 1)) + 1;
+		int randY = 2 * (int)(Math.random() * (size / 2 - 1)) + 1;
+		maze = MazeGenerator.generateMaze(size, size, randX, randY);
+		System.out.println(randX + " " + randY);
+	}
+
+	public static void main(String[] args) {
+		Map map = new Map(13);
+		map.print();
+	}
+
+	public void print() {
+		for (boolean[] row : maze) {
+			for (boolean b : row) {
+				if (b) System.out.print("* ");
+				else System.out.print("| ");
+			}
+			System.out.println();
+		}
+	}
 }
