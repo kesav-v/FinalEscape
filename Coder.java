86c7a4324ca5214d088878c7241b8b85951df8b6
Ofek Gila <ofek@theofekfoundation.org>
2017-03-30 13:27:19 -0700

    Add Inventory basic skeleton

--- Coder.java
+++ Coder.java
@@ -1,5 +1,9 @@
 public class Coder extends Character {
+
+	private Inventory inventory;
+
 	public Coder(Map map, int x, int y) {
 		super(map, x, y, "images/coder.png");
+		inventory = new Inventory();
 	}
 }
