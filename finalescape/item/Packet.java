0756760f3da00feafb84701f868869dec10175e5
Ofek Gila <ofek@theofekfoundation.org>
2017-04-04 14:50:15 -0700

    Fix Packet destroying walls

--- finalescape/item/Packet.java
+++ finalescape/item/Packet.java
@@ -1,5 +1,6 @@
 package finalescape.item;
 
+import finalescape.mapcomponent.MapComponent;
 import finalescape.mapcomponent.PacketProjectile;
 
 public class Packet extends ProjectileItem {
@@ -7,5 +8,14 @@ public class Packet extends ProjectileItem {
 	public Packet() {
 		super("Packet");
 		setProjectile(new PacketProjectile(this));
+	}
+
+	@Override
+	public boolean canPlaceOn(MapComponent component) {
+		if (component == null || !component.isSolid())
+			return true;
+		else if (component.isSolid())
+			overrideDestroy(false);
+		return false;
 	}
 }
