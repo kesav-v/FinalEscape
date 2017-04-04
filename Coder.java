a81d93635fed18e9fbb6e0cc94b8ad4158deb894
Ofek Gila <ofek@theofekfoundation.org>
2017-04-03 20:51:30 -0700

    Fix coder always holding boomerang bug

--- Coder.java
+++ Coder.java
@@ -5,7 +5,6 @@ public class Coder extends Character {
 	public Coder(Map map, int x, int y) {
 		super(map, x, y, "Coder", 5);
 		setColor(Color.BLUE);
-		getInventory().add(new Boomerang());
 	}
 
 	@Override
