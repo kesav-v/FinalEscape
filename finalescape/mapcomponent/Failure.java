3c46a6e2a03121c848be2d2b5a4cf1a0cdf14d6e
Ofek Gila <ofek@theofekfoundation.org>
2017-04-04 16:07:12 -0700

    Speed up failure over time

--- finalescape/mapcomponent/Failure.java
+++ finalescape/mapcomponent/Failure.java
@@ -6,13 +6,20 @@ import java.awt.Color;
 
 public class Failure extends Teacher {
 
+	private int elapsedTicks;
+
 	public Failure() {
 		super("Failure", null, 0);
+		setDelayInterval(15);
+		elapsedTicks = 0;
 	}
 
 	@Override
 	public void tick() {
 		tryMovingInDir(solveMazeDirection(getTarget()));
+		elapsedTicks++;
+		if (elapsedTicks % 500 == 0 && getDelayInterval() > 5)
+			setDelayInterval(getDelayInterval() - 1);
 	}
 
 	@Override
