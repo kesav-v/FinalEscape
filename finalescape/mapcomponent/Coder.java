3b0a63679213a13188423a45fdacac1975494667
Ofek Gila <ofek@theofekfoundation.org>
2017-04-06 17:02:25 -0700

    Add Coder javadoc

--- finalescape/mapcomponent/Coder.java
+++ finalescape/mapcomponent/Coder.java
@@ -4,6 +4,16 @@ import finalescape.map.Map;
 
 import java.awt.Color;
 
+/**
+ * The main {@link Character} of the {@link Map}, the {@code Coder} is controlled
+ * by the player.
+ *
+ * The goal of the {@code Coder} is to bring the {@link finalescape.item.Laptop}
+ * to the {@link Desk}.
+ *
+ * @author Ofek Gila
+ * @see Character
+ */
 public class Coder extends Character {
 
 	public Coder(Map map, int x, int y) {
@@ -12,6 +22,13 @@ public class Coder extends Character {
 	}
 
 	@Override
+	/**
+	 * Checks if can move to coordinate, picking up any {@link finalescape.item.Item}s
+	 * from any {@link ItemComponent}s it steps on.
+	 * @param  x x coordinate
+	 * @param  y y coordinate
+	 * @return   true if can move here, false otherwise
+	 */
 	public boolean canMoveHere(int x, int y) {
 		MapComponent componentThere = getMap().get(x, y);
 		Inventory inventory = getInventory();
@@ -27,6 +44,9 @@ public class Coder extends Character {
 	}
 
 	@Override
+	/**
+	 * When the {@code Coder} is destroyed, the game is lost.
+	 */
 	public void destroy() {
 		getMap().loseGame();
 	}
